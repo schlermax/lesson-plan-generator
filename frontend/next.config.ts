ebaa76cdb044c470d6ea2767b3083f2773be177c
Maxwell Schleicher <schlermax5@gmail.com>
2026-04-28 20:53:41 -0400

    repository path fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,8 +5,8 @@ const isProd = process.env.NEXT_PUBLIC_ENVIRONMENT === "production";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: isProd ? "export" : undefined,
-  basePath: isProd ? "/Next.js-Lesson-Plans" : "",
-  assetPrefix: isProd ? "/Next.js-Lesson-Plans/" : "",
+  basePath: isProd ? "/lesson-plan-generator" : "",
+  assetPrefix: isProd ? "/lesson-plan-generator/" : "",
   images: {
     unoptimized: isProd,
   },
